25b3403a97a484b953c03684283ba1cf4d01468c
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-17 18:43:41 +0200

    feat: enhance authentication flow with guest menu and update redirect route

---
+++ resources/js/components/GuestMenuContent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { DropdownMenuGroup, DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <!--
+ <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+-->
+    <DropdownMenuGroup>
+        <DropdownMenuItem :as-child="true">
+            <Link :href="route('login')" as="button"><span>Log in</span></Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem :as-child="true">
+            <Link :href="route('register')" as="button">
+                <span>Register</span>
+            </Link>
+        </DropdownMenuItem>
+    </DropdownMenuGroup>
+</template>
