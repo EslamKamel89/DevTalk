691e43c7b933cdba89492d24bdf81e0467130a6e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-21 16:04:14 +0200

    feat: add TopicResource for transforming topic data and update share method to use it

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\TopicResource;
 use App\Models\Topic;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
@@ -40,7 +41,7 @@ class HandleInertiaRequests extends Middleware {
         return [
             ...parent::share($request),
             'name' => config('app.name'),
-            "topics" => Topic::all(),
+            "topics" => TopicResource::collection(Topic::all()),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
