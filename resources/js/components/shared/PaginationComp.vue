9011c70558049b3bfc8037b3ce25a25e15704615
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-23 15:39:36 +0200

    refactor: simplify Pagination component by removing unused navigation links and updating conditional rendering

--- resources/js/components/shared/PaginationComp.vue
+++ resources/js/components/shared/PaginationComp.vue
@@ -6,7 +6,7 @@ const props = defineProps<{
 }>();
 </script>
 <template>
-    <nav aria-label="Page navigation example">
+    <nav v-if="links.length > 3">
         <ul class="inline-flex -space-x-px text-sm">
             <li v-for="link in links" :key="link.label">
                 <LinkComp
@@ -21,57 +21,6 @@ const props = defineProps<{
                     >{{ link.label }}</LinkComp
                 >
             </li>
-            <!--
-         <li>
-                <a
-                    href="#"
-                    class="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 ms-0 rounded-s-lg border-e-0 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >Previous</a
-                >
-            </li>
-            <li>
-                <a
-                    href="#"
-                    class="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >1</a
-                >
-            </li>
-            <li>
-                <a
-                    href="#"
-                    class="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >2</a
-                >
-            </li>
-            <li>
-                <a
-                    href="#"
-                    aria-current="page"
-                    class="flex items-center justify-center h-8 px-3 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                    >3</a
-                >
-            </li>
-            <li>
-                <a
-                    href="#"
-                    class="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >4</a
-                >
-            </li>
-            <li>
-                <a
-                    href="#"
-                    class="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >5</a
-                >
-            </li>
-            <li>
-                <a
-                    href="#"
-                    class="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >Next</a
-                >
-            </li>
-        --></ul>
+        </ul>
     </nav>
 </template>
